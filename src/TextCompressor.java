56ff89cf47480b8fccee12febe18000b7713ce66
williambeesley <wpbeesley@gmail.com>
2025-12-08 18:58:59 -0800

    vFinal. All cases passed + Bits chosen in command line!

--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -1,4 +1,3 @@
-import java.util.*;
 /******************************************************************************
  *  Compilation:  javac TextCompressor.java
  *  Execution:    java TextCompressor - < input.txt   (compress)
@@ -29,28 +28,75 @@ import java.util.*;
  *  @author Zach Blick, William Beesley
  */
 public class TextCompressor {
+    public static final int EOF = 128;
 
-    private static void compress() {
+    private static void compress(int BITS) {
         String input = BinaryStdIn.readString();
-        String[] parts = input.split(" ");
-        Map<String, Integer> freq = new HashMap<>();
-        for (String s : parts) {
-            freq.put(s, freq.getOrDefault(s, 0) + 1);
+        TST keys = new TST();
+        int iterator = 0;
+        int length = input.length();
+        String prefix;
+        int code;
+        int key_number = EOF + 1;
+        int max = (int) Math.pow(2, BITS);
+        while (iterator < length) {
+            // Start by finding the longest prefix to save bits and the corresponding code
+            prefix = keys.getLongestPrefix(input, iterator);
+            code = keys.lookup(prefix);
+            // Output the code
+            BinaryStdOut.write(code, BITS);
+            // Look ahead to create the new codes
+            iterator += prefix.length();
+            // Stay in bounds since we just changed iterator
+            if (iterator >= length) {
+                break;
+            }
+            if (key_number < max) {
+                keys.insert(prefix + input.charAt(iterator), key_number);
+            }
+            key_number++;
         }
-
+        BinaryStdOut.write(EOF, BITS);
         BinaryStdOut.close();
     }
 
-    private static void expand() {
-
-        // TODO: Complete the expand() method
-
+    private static void expand(int BITS) {
+        // Max number of keys is 2 ^ BITS - 128 so just use 2 ^ BITS to be safe
+        String[] keys = new String[(int) Math.pow(2, BITS)];
+        int key_number = EOF + 1;
+        int max = (int) Math.pow(2, BITS);
+        // Add ASCII to the map
+        for (int i = 0; i < 128; i++) {
+            keys[i] = (char) (i) + "";
+        }
+        int input = BinaryStdIn.readInt(BITS);
+        while (input != EOF) {
+            // Write out the string
+            String str1 = keys[input];
+            BinaryStdOut.write(str1);
+            // Look ahead to the next string
+            int next_input = BinaryStdIn.readInt(BITS);
+            String str2 = keys[next_input];
+            // Edge case! Next string isn't yet in our map
+            if (str2 == null) {
+                str2 = str1 + str1.charAt(0);
+            }
+            // Add new code to the map, only include first character of the next string
+            if (key_number < max) {
+                if (next_input != EOF) {
+                    keys[key_number] = keys[input] + str2.charAt(0);
+                }
+                key_number++;
+            }
+            input = next_input;
+        }
         BinaryStdOut.close();
     }
 
     public static void main(String[] args) {
-        if      (args[0].equals("-")) compress();
-        else if (args[0].equals("+")) expand();
+        int BITS = Integer.parseInt(args[1]);
+        if      (args[0].equals("-")) compress(BITS);
+        else if (args[0].equals("+")) expand(BITS);
         else throw new IllegalArgumentException("Illegal command line argument");
     }
 }
